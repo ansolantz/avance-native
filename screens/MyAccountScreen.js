2da0acdf47e313aa8dccf0bbb9894b4930e66b41
Ann-Sofi Lantz <Anso@Anso.local>
2019-05-22 23:43:24 +0200

    Edited my account

--- screens/MyAccountScreen.js
+++ screens/MyAccountScreen.js
@@ -7,7 +7,17 @@ import auth from '../lib/auth-services'
 import axios from 'axios';
 
 export default class MyAccountScreen extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: 'username',
+      displayName: 'displayName',
+      email: 'email',
+      age: 'age',
+      weight: 'weight',
+      height: 'height'
+    };
+  }
   static navigationOptions = {
     // header: null,
     title: 'Uppdate User'
@@ -15,7 +25,6 @@ export default class MyAccountScreen extends Component {
   render() {
     return (
       <Container>
-
         <Content>
           <Form>
             <Item floatingLabel>
@@ -27,8 +36,28 @@ export default class MyAccountScreen extends Component {
               <Input onChangeText={(password) => this.setState({ password })} />
             </Item>
             <Item floatingLabel last>
+              <Label>Name</Label>
+              <Input onChangeText={(displayName) => this.setState({ displayName })} />
+            </Item>
+
+            <Item floatingLabel last>
               <Label>Email</Label>
               <Input onChangeText={(email) => this.setState({ email })} />
+            </Item>
+
+            <Item floatingLabel last>
+              <Label>Age</Label>
+              <Input onChangeText={(age) => this.setState({ age })} />
+            </Item>
+
+            <Item floatingLabel last>
+              <Label>Wight</Label>
+              <Input onChangeText={(weight) => this.setState({ weight })} />
+            </Item>
+
+            <Item floatingLabel last>
+              <Label>Height</Label>
+              <Input onChangeText={(height) => this.setState({ height })} />
             </Item>
 
             <Button block info onPress={this.addUser}>
