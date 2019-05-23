9a69e877a85f9a196b20a8e4cbf3b31d701e1182
Ann-Sofi Lantz <Anso@Anso.local>
2019-05-23 19:54:27 +0200

    Updated api with backend

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Alert } from 'react-native';
 import {
   Text, Container, Content, Form, Item, Input, Label, Button,
 } from 'native-base';
@@ -10,8 +11,8 @@ export default class SignupScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: 'Username',
-      password: '****'
+      username: '',
+      password: ''
     };
   }
 
@@ -20,43 +21,40 @@ export default class SignupScreen extends Component {
     title: 'Signup'
   };
 
+  showError = () => {
+    Alert.alert(
+      'Alert Title',
+      'Error creating user',
+      [
+        { text: 'OK', onPress: () => console.log('OK Pressed') },
+      ],
+      { cancelable: false },
+    );
+
+  }
+
+
   addUser = () => {
     console.log('ADD USER!')
     console.log(this.state)
     const { username, password } = this.state;
-    auth.signup({ username, password });
-    //auth.updateUser({ username, password });
-    this.props.navigation.navigate('Dashboard')
+    // try {
+    auth.signup({ username, password })
+      .then(() => {
+        this.setState({ username: '', password: '' })
+        this.props.navigation.navigate('Dashboard')
+      })
+      .catch(err => console.log(err));
+
+    // } catch (error) {
+    //   this.showError();
+    // }
 
   }
 
-  updateUser = () => {
-    console.log('UPPDATE USER')
-
-    //Home
-    axios.put('http://192.168.1.118:5000/user/update/5ce3f2262c5b7d66e98f2def', {
-      displayName: 'Ansoooo22222',
-      email: 'anso@test.com2222333'
-    })
-    //axios.put('http://192.168.65.215:5000/user/update/5ce3f2262c5b7d66e98f2def', {
-
-    //Phone
-
-    // axios.get('http://exaple.com')
-    //   // axios.put('http://172.20.10.3:5000/user/update/5ce3f2262c5b7d66e98f2def', {
-    //   //   displayName: 'Ansoooo22222',
-    //   //   email: 'anso@test.com2222'
-    //   // })
-    //   .then(res => console.log(res.data))
-    //   .catch((err) => console.log(err));
-
-    this.props.navigation.navigate('Dashboard')
-
-  }
   render() {
     return (
       <Container>
-
         <Content>
           <Form>
             <Item floatingLabel>
@@ -65,19 +63,16 @@ export default class SignupScreen extends Component {
             </Item>
             <Item floatingLabel last>
               <Label>Password</Label>
-              <Input onChangeText={(password) => this.setState({ password })} />
+              <Input secureTextEntry={true} onChangeText={(password) => this.setState({ password })} />
             </Item>
-
+            <Text> </Text>
             <Button block info onPress={this.addUser}>
               <Text>Signup</Text>
             </Button>
-
+            <Text> </Text>
             <Button block info onPress={() => auth.logout()}>
               <Text>Logout</Text>
             </Button>
-
-
-
 
           </Form>
         </Content>
