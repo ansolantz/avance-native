3f9b73678053434f925ba0aa3049e7ad909531b5
Ann-Sofi Lantz <Anso@Anso.local>
2019-05-24 22:34:42 +0200

    Working progress

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -14,53 +14,48 @@ export default class DashboardScreen extends Component {
 
       <Container>
         <Content>
-
           <List>
-
-            <LinearGradient colors={['#dfe3ee', '#8b96b3', '#6c8acc']} style={{ margin: 5, borderRadius: 15}} >
+          <LinearGradient start={[1,1]} end={[0,0]} colors={['#afbbda', '#8b96b3', '#6c8acc']} style={{ margin: 5, borderRadius: 15 }} >
               <ListItem height={100} button onPress={() => this.props.navigation.navigate('Recommendation')}>
                 <Thumbnail square size={80} source={require('../assets/images/water-icon.png')} />
-                <Text>Hidration</Text>
+                <Text style={{ color: '#ffffff'}}>Hidration</Text>
               </ListItem>
             </LinearGradient>
 
-            <LinearGradient colors={['#ebf0e6', '#bdd0a6', '#8fc170']} style={{ margin: 5, borderRadius: 15}} >
+           <LinearGradient start={[1,1]} end={[0,0]} colors={['#d1dec5', '#bdd0a6', '#8fc170']} style={{ margin: 5, borderRadius: 15 }} >
               <ListItem height={100} >
                 <Thumbnail square size={80} source={require('../assets/images/apple-icon.png')} />
-                <Text>Vitamins</Text>
+                <Text style={{ color: '#ffffff' }}>Vitamins</Text>
               </ListItem>
             </LinearGradient>
 
-            <LinearGradient colors={['#e5d7e8', '#c899d4', '#8f4ea0']} style={{ margin: 5, borderRadius: 15}} >
+            <LinearGradient start={[1,1]} end={[0,0]} colors={['#d8bfde', '#c899d4', '#8f4ea0']} style={{ margin: 5, borderRadius: 15 }} >
               <ListItem height={100}>
                 <Thumbnail square size={80} source={require('../assets/images/walking-icon.png')} />
-                <Text style={{ padding: 5 }}>Move</Text>
+                <Text style={{ color: '#ffffff' }} style={{ padding: 5 }}>Move</Text>
               </ListItem>
             </LinearGradient>
 
-
-            <LinearGradient colors={['transparent','rgba(0,0,0,0.6)']} style={{ margin: 5, borderRadius: 15}} >
-            <ListItem height={100}>
+            <LinearGradient start={[1,1]} end={[0,0]} colors={['transparent', 'rgba(0,0,0,0.6)']} style={{ margin: 5, borderRadius: 15 }} >
+              <ListItem height={100}>
                 <Thumbnail square size={80} source={require('../assets/images/food-icon.png')} />
-                <Text style={{ padding: 5 }}>Food</Text>
+                <Text style={{ color: '#ffffff' }} style={{ padding: 5 }}>Food</Text>
               </ListItem>
             </LinearGradient>
 
-             <LinearGradient colors={['transparent','rgba(0,0,0,0.6)']} style={{ margin: 5, borderRadius: 15}} >
-             <ListItem height={100}>
+            <LinearGradient start={[1,1]} end={[0,0]} colors={['transparent', 'rgba(0,0,0,0.6)']} style={{ margin: 5, borderRadius: 15 }} >
+              <ListItem height={100}>
                 <Thumbnail square size={80} source={require('../assets/images/water-icon.png')} />
-                <Text>Sleep</Text>
+                <Text style={{ color: '#ffffff' }}>Sleep</Text>
               </ListItem>
             </LinearGradient>
 
-             <LinearGradient colors={['transparent','rgba(0,0,0,0.6)']} style={{ margin: 5, borderRadius: 15}} >
-             <ListItem height={100}>
+            <LinearGradient start={[1,1]} end={[0,0]} colors={['transparent', 'rgba(0,0,0,0.6)']} style={{ margin: 5, borderRadius: 15 }} >
+              <ListItem height={100}>
                 <Thumbnail square size={80} source={require('../assets/images/water-icon.png')} />
-                <Text>Focus</Text>
+                <Text style={{ color: '#ffffff' }}>Focus</Text>
               </ListItem>
             </LinearGradient>
-          
-           
           </List>
         </Content>
       </Container>
@@ -68,6 +63,4 @@ export default class DashboardScreen extends Component {
     );
   }
 }
-
-
 
