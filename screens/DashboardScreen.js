f6bfe3f5199d1d69bc373fdb4f6719147b3a3775
Ann-Sofi Lantz <Anso@Anso.local>
2019-05-24 20:33:30 +0200

    working progress

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -17,45 +17,53 @@ export default class DashboardScreen extends Component {
 
           <List>
 
-
-            <LinearGradient colors={['rgba(0,0,0,0.8)', 'transparent']} >
-              <ListItem height={90} button onPress={() => this.props.navigation.navigate('Recommendation')}>
+            <LinearGradient colors={['#dfe3ee', '#8b96b3', '#6c8acc']} style={{ margin: 5, borderRadius: 15}} >
+              <ListItem height={100} button onPress={() => this.props.navigation.navigate('Recommendation')}>
                 <Thumbnail square size={80} source={require('../assets/images/water-icon.png')} />
+                <Text>Hidration</Text>
+              </ListItem>
+            </LinearGradient>
+
+            <LinearGradient colors={['#ebf0e6', '#bdd0a6', '#8fc170']} style={{ margin: 5, borderRadius: 15}} >
+              <ListItem height={100} >
+                <Thumbnail square size={80} source={require('../assets/images/apple-icon.png')} />
                 <Text>Vitamins</Text>
               </ListItem>
             </LinearGradient>
 
+            <LinearGradient colors={['#e5d7e8', '#c899d4', '#8f4ea0']} style={{ margin: 5, borderRadius: 15}} >
+              <ListItem height={100}>
+                <Thumbnail square size={80} source={require('../assets/images/walking-icon.png')} />
+                <Text style={{ padding: 5 }}>Move</Text>
+              </ListItem>
+            </LinearGradient>
 
-            <ListItem height={90}>
-              <Thumbnail square size={80} source={require('../assets/images/apple-icon.png')} />
-              <Text>Vitamins</Text>
-            </ListItem>
-            <ListItem height={90}>
-              <Thumbnail square size={80} source={require('../assets/images/walking-icon.png')} />
-              <Text style={{ padding: 5 }}>Move</Text>
-            </ListItem>
-            <ListItem style={{ backgroundColor: 'rgba(0,0,0,0.05)' }}>
-              <Thumbnail square size={80} source={require('../assets/images/food-icon.png')} />
-              <Text style={{ padding: 5 }}>Food</Text>
-            </ListItem>
-            <ListItem style={{ backgroundColor: 'rgba(0,0,0,0.05)' }}>
-              <Thumbnail square size={80} source={require('../assets/images/water-icon.png')} />
-              <Text>Sleep</Text>
-            </ListItem>
-            <ListItem style={{ backgroundColor: 'rgba(0,0,0,0.05)' }}>
-              <Thumbnail square size={80} source={require('../assets/images/water-icon.png')} />
-              <Text>Focus</Text>
-            </ListItem>
 
-            {/* 
-              <Icon name='walking' />
-              <Icon name='ios-water' />
-              <Icon name='md-water' /> */}
+            <LinearGradient colors={['transparent','rgba(0,0,0,0.6)']} style={{ margin: 5, borderRadius: 15}} >
+            <ListItem height={100}>
+                <Thumbnail square size={80} source={require('../assets/images/food-icon.png')} />
+                <Text style={{ padding: 5 }}>Food</Text>
+              </ListItem>
+            </LinearGradient>
 
+             <LinearGradient colors={['transparent','rgba(0,0,0,0.6)']} style={{ margin: 5, borderRadius: 15}} >
+             <ListItem height={100}>
+                <Thumbnail square size={80} source={require('../assets/images/water-icon.png')} />
+                <Text>Sleep</Text>
+              </ListItem>
+            </LinearGradient>
+
+             <LinearGradient colors={['transparent','rgba(0,0,0,0.6)']} style={{ margin: 5, borderRadius: 15}} >
+             <ListItem height={100}>
+                <Thumbnail square size={80} source={require('../assets/images/water-icon.png')} />
+                <Text>Focus</Text>
+              </ListItem>
+            </LinearGradient>
+          
+           
           </List>
-        </Content >
-      </Container >
-
+        </Content>
+      </Container>
 
     );
   }
